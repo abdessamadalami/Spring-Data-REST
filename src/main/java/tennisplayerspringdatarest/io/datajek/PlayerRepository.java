f3ea9a13cf1673bc360b4d1daa5af3d396512d3a
abdessamadalami <eloualialamiabdessamad@gmail.com>
2023-11-27 13:06:36 +0100

    Spring Data Rest

---
+++ src/main/java/tennisplayerspringdatarest/io/datajek/PlayerRepository.java
@@ -0,0 +1,6 @@
+package tennisplayerspringdatarest.io.datajek;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerRepository extends JpaRepository<Player,Integer> {
+}
---
+++ src/test/java/tennisplayerspringdatarest/io/datajek/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tennisplayerspringdatarest.io.datajek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
